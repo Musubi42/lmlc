56a9c44cd22d9ebf842b816c550f7253d8709206
Trusgnach Arthur <trusgnach.arthur@gmail.com>
2023-10-13 22:19:20 +0200

    countdown

--- app.vue
+++ app.vue
@@ -1,32 +1,52 @@
 <template>
-  <div class="container">
-    <div class="logo-container">
-      <img class="logo" src="~/assets/logo_lmlc.png" alt="Logo lmlccommunication" />
+  <div class="container mx-auto">
+    <!-- Logo -->
+    <div class="grid grid-rows-4 gap-4">
+    <div class="w-1/3 mx-auto my-8">
+      <img src="~/assets/logo_lmlc.png" alt="Logo lmlccommunication" />
     </div>
-    <div class="countdown-container">
-      <div class="countdown">
-        <p class="countdown-text">{{ timeRemaining }}</p>
+
+    <!-- Countdown -->
+    <div class="countdown-container my-8">
+      <div class="flex w-full  bg-[#D9D9D9] p-4 rounded h-64 justify-center items-center ">
+        <div class="relative z-10 text-center w-full">
+          <div class="flex justify-around w-full p-10">
+            <div> <p class="text-6xl font-bold">{{ temps.jours }}</p></div>
+            <div> <p class="text-6xl font-bold">{{ temps.hours }}</p></div>
+            <div> <p class="text-6xl font-bold">{{ temps.minutes }}</p></div>
+            <div> <p class="text-6xl font-bold">{{ temps.secondes }}</p></div>
+          </div>
+        </div>
       </div>
     </div>
+
+    <!-- Contact -->
+    <div class="contact-container my-8 pl-20">
+      <p class="font-bold">QUESTION & </p> 
+      <p class="font-bold">
+      NEW BUISINESS</p>
+      <p>contact@lmlccommunication.fr</p>
+      <p class="font-bold">JOB</p>
+      <p>talents@lmlccommunication.fr</p>
+    </div>
   </div>
+</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      targetDate: new Date(), // Initialisez avec la date d'aujourd'hui
-      timeRemaining: "Calcul en cours...", // Affiche le temps restant
-      temps: 'calcul'
+      targetDate: new Date(),
+      timeRemaining: "",
+      temps: ''
     };
   },
   mounted() {
-    // Calcule la date cible (le 25 novembre de cette année)
-    this.targetDate.setMonth(10); // 10 représente novembre (0-indexed)
+    this.targetDate.setMonth(10);
     this.targetDate.setDate(25);
     this.targetDate.setHours(0);
     this.targetDate.setMinutes(0);
-    // Met à jour l'affichage toutes les secondes
     this.updateCountdown();
     setInterval(this.updateCountdown, 1000);
   },
@@ -44,41 +64,9 @@ export default {
         const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
         this.timeRemaining = `${days}  ${hours}  ${minutes}  ${seconds} `;
-        this.temps = { 'jours': days, 'hours': hours, 'minutes': minutes, 'seconde': seconds }
+        this.temps = { 'jours': days, 'hours': hours, 'minutes': minutes, 'secondes': seconds }
       }
     },
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
-
-.logo-container {
-  margin-bottom: 20px;
-}
-
-.logo {
-  width: 900px;
-}
-
-.countdown-container {
-  text-align: center;
-}
-
-.countdown {
-  background-color: #ccc; /* Rectangle gris pour le compteur */
-  padding: 20px; /* Espacement autour du compteur gris */
-  border-radius: 5px; /* Coins arrondis pour le compteur gris */
-}
-
-.countdown-text {
-  font-size: 50px; /* Taille de police en gros */
-}
-</style>
