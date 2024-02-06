a8cde57d638380d960293873402ba689a9aa707f
Trusgnach Arthur <trusgnach.arthur@gmail.com>
2024-02-06 23:01:33 +0100

    debut monin

--- pages/work.vue
+++ pages/work.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="bg-cover bg-center bg-fixed bg-interactive-whiteboard">
+  <div id="caroussel" class="bg-cover bg-center bg-fixed bg-black">
     <div class="relative slide w-full h-screen ">
       <div class="carousel-inner relative overflow-hidden h-screen z-1">
         <div v-for="(img, i) in images" :id="`slide-${i}`" :key="i" :class="`${active === i ? 'active' : 'left-full'}`"
           class="carousel-item inset-0 relative h-screen transform transition-all duration-1000 ease-in">
           <div :style="{ backgroundColor: img.backgroundColor }">
-          <img class="block w-full" :src="img.image" alt=${img.description} />
-          <div id="carousel-text"
-            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-center">
-            <h2 class="text-9xl font-bold">{{ img.title }}</h2>
-            <p class="text-2xl">{{ img.description }}</p>
+
+            <div id="carousel-text"
+              class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 text-center">
+              <h2 class="text-9xl font-bold text-white ">{{ img.title }}</h2>
+              <p class="text-2xl text-white ">{{ img.description }}</p>
+            </div>
           </div>
-        </div>
         </div>
       </div>
     </div>
@@ -19,11 +19,10 @@
     <button type="button"
       class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
       data-carousel-prev @click="goPrev">
-      <span
-        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+      <span class="border-0 text-white rounded-full p-2 ml-2 active:bg-rose-neon/50">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+          <path transform="scale(-1, 1) translate(-24, 0)" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M9 5l7 7-7 7" />
         </svg>
         <span class="sr-only">Previous</span>
       </span>
@@ -31,85 +30,30 @@
     <button type="button"
       class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
       data-carousel-next @click="goNext">
-      <span
-        class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-        <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+      <span class="border-0 text-white rounded-full p-2 ml-2 active:bg-rose-neon/50">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
         </svg>
         <span class="sr-only">Next</span>
       </span>
     </button>
-    
-  <div
-    class="fixed-background bg-cover bg-center h-screen bg-fixed flex items-center justify-center bg-interactive-whiteboard mt-96">
-    <div class="container fixed-background-content relative z-10">
-      <div class="flex flex-row">
-        <div class="basis-1/2">
-          <div id="monElement" class="flotter-container ">
-            <img src="/assets/images/sac-bleu.png" alt="Image à flotter" class="flotter-image">
-          </div>
-        </div>
-        <div class="basis-1/4"></div>
-        <div class="basis-1/2"></div>
-      </div>
-
-      <div class="flex sticky text-container text-black text-4xl font-bold z-1">
-        <p class="text-black">
-          La quête infinie de l'humanité pour comprendre l'univers trouve son expression la plus audacieuse dans
-          l'exploration spatiale. Des sondes qui parcourent les confins du système solaire aux télescopes puissants qui
-          scrutent les mystères des étoiles lointaines, chaque mission spatiale élargit notre compréhension de l'espace
-          et de notre place en son sein.
-        </p>
-      </div>
-
-      <div class="flex flex-row">
-        <div class="basis-1/2"></div>
-        <div class="basis-1/4"></div>
-        <div class="basis-1/2"></div>
-      </div>
-
-      <div class="text-container text-black text-4xl font-bold my-52">
-        <p class="text-black">
-          Dans un monde confronté à des défis environnementaux croissants, la durabilité est devenue un impératif. De la
-          réduction des émissions de carbone à la préservation de la biodiversité, les efforts pour créer un avenir plus
-          durable s'étendent à tous les domaines de la société. Les choix que nous faisons aujourd'hui façonnent la
-          qualité de vie des générations futures.
-        </p>
-      </div>
-
-      <div class="flex flex-row">
-        <div class="basis-1/2"></div>
-        <div class="basis-1/4">
-          <div id="monElement" class="flotter-container ">
-            <img src="/assets/images/sac-bleu.png" alt="Image à flotter" class="flotter-image">
-          </div>
-        </div>
-        <div class="basis-1/2"></div>
-      </div>
-
-      <div class="text-container text-black text-4xl font-bold mb-96">
-        <p class="text-black texts">
-          L'exploration spatiale a captivé l'imagination de l'humanité depuis des décennies. Des premiers pas de l'homme
-          sur la Lune aux missions robotiques qui explorent les confins de notre système solaire, l'aventure spatiale
-          continue de repousser les limites de notre compréhension. Chaque découverte, chaque cliché de planètes
-          lointaines, nous offre un aperçu fascinant de l'univers infini qui nous entoure.
-        </p>
-      </div>
-
-      <div class="flex flex-row">
-        <div class="basis-1/2">
-          <div id="monElement" class="flotter-container">
-            <img src="/assets/images/sac-bleu.png" alt="Image à flotter" class="flotter-image">
-          </div>
-        </div>
-        <div class="basis-1/4"></div>
-        <div class="basis-1/2"></div>
-      </div>
-
-      <div class="h-96"></div>
-    </div>
   </div>
+
+  <div id="client" class="container mx-auto p-8 h-screen">
+    <div class="flex items-center mb-8">
+      <div>
+      <h1 class="text-9xl font-bold mr-4">MONIN</h1>
+      <p class="text-lg mb-4">Leader mondial des sirops et première référence du <br> monde de la mixologie, notre agence
+        accompagne<br> l’entreprise familiale dans des projets de <br> brandcontent et de social media.</p>
+      <p class="text-base mb-4 ">
+        social media<br>
+        community management<br>
+        production audiovisuelle<br>
+        shooting photo
+      </p>
+    </div>
+      <img src="/monin.gif" alt="GIF" class="h-1/4 ml-auto" loop>
+    </div>
   </div>
 </template>
 
@@ -117,6 +61,7 @@
 export default {
   data() {
     return {
+      scrollPosition: 0,
       currentSlide: 0,
       hideCarousel: false,
       imageScale: 1,
@@ -124,31 +69,34 @@ export default {
       imageTop: 0,
       active: 0,
       images: [
-        { image: "/services.png", title: "Titre 1", description: "Description 1", alt: "truc", backgroundColor : "#974dff" },
-        { image: "/talents.png", title: "Titre 2", description: "Description 2", alt: "truc", backgroundColor : "#974dff" },
-        { image: "/work.png", title: "Titre 3", description: "Description 3", alt: "truc", backgroundColor : "#974dff" },
-        { image: "/footer-job.png", title: "Titre 4", description: "Description 4", alt: "truc", backgroundColor : "#974dff" },
+        { title: "MONIN", description: "mixologie", alt: "truc", backgroundColor: "#974dff" },
+        { title: "CAPITALE EUROPEENNE DE LA CULTURE", description: "", alt: "truc", backgroundColor: "#974dff" },
+        {  title: "Titre 3", description: "Description 3", alt: "truc", backgroundColor: "#974dff" },
+        {  title: "Titre 4", description: "Description 4", alt: "truc", backgroundColor: "#974dff" },
       ],
       test: 0
     };
   },
   methods: {
     handleScroll() {
-      const threshold = 50;
-      const scrollY = window.scrollY;
+    this.scrollPosition = window.scrollY;
 
-      if (scrollY > threshold && !this.hideCarousel) {
-        this.hideCarousel = true;
-        this.imageScale = 0.3;
-        this.imageLeft = -35;
-        this.imageTop = -10;
-      } else if (scrollY <= threshold && this.hideCarousel) {
-        this.hideCarousel = false;
-        this.imageScale = 1;
-        this.imageLeft = 0;
-        this.imageTop = 0;
-      }
-    },
+    // Check if the user has scrolled to a certain position to trigger the scroll to the client section
+    const clientSectionOffset = document.getElementById("client").offsetTop;
+    if (this.scrollPosition >= clientSectionOffset - window.innerHeight / 2) {
+      this.scrollToClientSection();
+
+      // Remove the scroll event listener after triggering the scroll
+      window.removeEventListener("scroll", this.handleScroll);
+    }
+  },
+
+  scrollToClientSection() {
+    const clientSection = document.getElementById("client");
+
+    // Use smooth scrolling to scroll to the client section
+    clientSection.scrollIntoView({ behavior: "smooth" });
+  },
 
     goPrev() {
       console.log("test");
@@ -171,11 +119,11 @@ export default {
     }
   },
   mounted() {
-    window.addEventListener("scroll", this.handleScroll);
-  },
-  beforeDestroy() {
-    window.removeEventListener("scroll", this.handleScroll);
-  },
+  window.addEventListener("scroll", this.handleScroll);
+},
+beforeDestroy() {
+  window.removeEventListener("scroll", this.handleScroll);
+},
 };
 </script>
 
