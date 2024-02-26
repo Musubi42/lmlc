9fe23a85485c32e054b257d61dedf8662e18f82c
lmlc-leo <lmlccommunication@gmail.com>
2024-02-26 20:40:57 +0100

    Fix tableau appear

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
       >
         <div id="tableau" class="w-screen font-extralight h-[90%] md:h-full">
         <!-- TODO: Cette façon de faire fait pété le tout -->
-          <tableauTest v-if="!IntroAnimation" v-cursorAnimation :key="componentKey" />
+          <tableauTest v-if="!IntroAnimation" v-cursorAnimation  />
           <!-- <tableauTest v-if="true" v-cursorAnimation :key="componentKey" /> -->
         </div>
         <!-- TODO : Changer où je trigger la fnc isScrolling -->
@@ -148,6 +148,8 @@ export default {
 
     // Des fois, si je met un clg le if s'exec bien
     if (!firstVisit.value) {
+      console.log("oui");
+      // this.componentKey++;
       isIntroAnimation.value = false;
       IntroAnimation.value = false;
     }
