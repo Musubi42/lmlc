a93a638acdc11169f1889baaf19ba48a5998807c
lmlc-leo <lmlccommunication@gmail.com>
2024-03-07 17:42:07 +0100

    WIP

--- components/Monin.vue
+++ components/Monin.vue
@@ -6,176 +6,179 @@
     </div>
 
     <!-- <div> -->
-      <div class="flex flex-col lg:flex-row container mx-auto p-8 h-full lg:h-screen">
-        <div class="flex-grow flex flex-col justify-end 2xl:mb-20 lg:mb-20 lg:order-1 order-2">
-          <h1 class="2xl:text-9xl lg:text-8xl text-5xl font-bold 2xl:mr-4 lg:mr-4">
-            {{ t("TitleMonin") }}
-          </h1>
-          <p class="lg:text-2xl text-base mb-4">
-            {{ t("DescriptionMonin[0]") }} <br />
-            {{ t("DescriptionMonin[1]") }} <br />
-            {{ t("DescriptionMonin[2]") }}<br />
-            {{ t("DescriptionMonin[3]") }}
-          </p>
-          <p class="text-base font-extralight">
-            {{ t("WorkMonin[0]") }} <br />
-            {{ t("WorkMonin[1]") }} <br />
-            {{ t("WorkMonin[2]") }}<br />
-            {{ t("WorkMonin[3]") }}
-          </p>
-        </div>
-        <div
-  class="md:flex hidden justify-center lg:justify-end 2xl:w-2/5 lg:w-2/5 relative 2xl:mt-8  mr-10 lg:order-2 order-1"
->
-  <img src="/monin.gif" alt="GIF" class="object-cover mx-auto lg:h-5/6" />
-</div>
+    <div class="flex flex-col lg:flex-row container mx-auto p-8 h-full lg:h-screen">
+      <div
+        class="flex-grow flex flex-col justify-end 2xl:mb-20 lg:mb-20 lg:order-1 order-2"
+      >
+        <h1 class="2xl:text-9xl lg:text-8xl text-5xl font-bold 2xl:mr-4 lg:mr-4">
+          {{ t("TitleMonin") }}
+        </h1>
+        <p class="lg:text-2xl text-base mb-4">
+          {{ t("DescriptionMonin[0]") }} <br />
+          {{ t("DescriptionMonin[1]") }} <br />
+          {{ t("DescriptionMonin[2]") }}<br />
+          {{ t("DescriptionMonin[3]") }}
+        </p>
+        <p class="text-base font-extralight">
+          {{ t("WorkMonin[0]") }} <br />
+          {{ t("WorkMonin[1]") }} <br />
+          {{ t("WorkMonin[2]") }}<br />
+          {{ t("WorkMonin[3]") }}
+        </p>
+      </div>
+      <div
+        class="md:flex hidden justify-center lg:justify-end 2xl:w-2/5 lg:w-2/5 relative 2xl:mt-8 mr-10 lg:order-2 order-1"
+      >
+        <img src="/monin.gif" alt="GIF" class="object-cover mx-auto lg:h-5/6" />
+      </div>
       <!-- </div> -->
       <a id="Bottom"></a>
     </div>
 
     <div>
-    <a id="slide2"></a>
-    <div class="hidden md:flex containerDE horizontal h-screen">
-      <div class="panell h-screen">
-        <video
-          class="lg:h-[75%] flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-          autoplay
-          loop
-          muted
-          playsinline
-        >
-          <source :src="`/video/1_desktop.webm`" type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
+      <a id="slide2"></a>
+      <!-- Desktop -->
+      <div v-if="isMobile === false" class="hidden md:flex containerDE horizontal h-screen">
+        <div class="panell h-screen">
+          <video
+            class="lg:h-[75%] flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            autoplay
+            loop
+            muted
+            playsinline
+          >
+            <source :src="`/video/1_desktop.webm`" type="video/webm" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div class="panell h-screen">
+          <video
+            class="lg:h-[75%] flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            autoplay
+            loop
+            muted
+            playsinline
+          >
+            <source :src="`/video/2_desktop.webm`" type="video/webm" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div class="panell h-screen">
+          <video
+            class="lg:h-[75%] h-screen flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            autoplay
+            loop
+            muted
+            playsinline
+          >
+            <source :src="`/video/3_desktop.webm`" type="video/webm" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div class="panell h-screen">
+          <video
+            class="lg:h-[75%] flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            autoplay
+            loop
+            muted
+            playsinline
+          >
+            <source :src="`/video/4_desktop.webm`" type="video/webm" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div class="panell h-screen">
+          <video
+            class="lg:h-[75%] flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            autoplay
+            loop
+            muted
+            playsinline
+          >
+            <source :src="`/video/5_desktop.webm`" type="video/webm" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <div class="panell h-screen">
+          <video
+            class="lg:h-[75%] flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+            autoplay
+            loop
+            muted
+            playsinline
+          >
+            <source :src="`/video/6_desktop.webm`" type="video/webm" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
       </div>
-      <div class="panell h-screen">
-        <video
-          class="lg:h-[75%] flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-          autoplay
-          loop
-          muted
-          playsinline
-        >
-          <source :src="`/video/2_desktop.webm`" type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-      <div class="panell h-screen">
-        <video
-          class="lg:h-[75%] h-screen flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-          autoplay
-          loop
-          muted
-          playsinline
-        >
-          <source :src="`/video/3_desktop.webm`" type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-      <div class="panell h-screen">
-        <video
-          class="lg:h-[75%] flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-          autoplay
-          loop
-          muted
-          playsinline
-        >
-          <source :src="`/video/4_desktop.webm`" type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-      <div class="panell h-screen">
-        <video
-          class="lg:h-[75%] flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-          autoplay
-          loop
-          muted
-          playsinline
-        >
-          <source :src="`/video/5_desktop.webm`" type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-      <div class="panell h-screen">
-        <video
-          class="lg:h-[75%] flex content-center items-center relative mx-7 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-          autoplay
-          loop
-          muted
-          playsinline
-        >
-          <source :src="`/video/6_desktop.webm`" type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
-    </div>
 
-    <!-- mobile -->
-    <div class="md:hidden flex overflow-x-auto items-center gap-10 relative">
-      <div class="flex flex-nowrap">
-        <div class="w-[75%] flex-shrink-0 h-full mx-4 mb-4">
-          <video class="flex" autoplay loop muted playsinline>
-            <source :src="`/video/1_mobile_high.mp4`" type="video/mp4" />
-            <source :src="`/video/1_mobile_medium.mp4`" type="video/mp4" />
-            <source :src="`/video/1_mobile_low.mp4`" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        <button
-          class="absolute bottom-1/2 right-0 transform translate-x-1/2 z-30 flex items-center justify-center mr-16 group focus:outline-none"
-        >
-          <span class="border-0 rounded-full p-2 active:bg-rose-neon/50 -rotate-90">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 16 16"
-              class="bounce"
-            >
-              <path
-                fill="currentColor"
-                fill-rule="evenodd"
-                d="M2.22 5.22a.75.75 0 0 0 0 1.06l5.252 5.252a.75.75 0 0 0 1.06 0l5.252-5.252a.75.75 0 1 0-1.06-1.06L8.001 9.94L3.28 5.22a.75.75 0 0 0-1.06 0"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </span>
-        </button>
+      <!-- mobile -->
+      <div v-if="isMobile === true" class="md:hidden flex overflow-x-auto items-center gap-10 relative">
+        <div class="flex flex-nowrap">
+          <div class="w-[75%] flex-shrink-0 h-full mx-4 mb-4">
+            <video class="flex" autoplay loop muted playsinline>
+              <source :src="`/video/1_mobile_high.mp4`" type="video/mp4" />
+              <source :src="`/video/1_mobile_medium.mp4`" type="video/mp4" />
+              <source :src="`/video/1_mobile_low.mp4`" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <button
+            class="absolute bottom-1/2 right-0 transform translate-x-1/2 z-30 flex items-center justify-center mr-16 group focus:outline-none"
+          >
+            <span class="border-0 rounded-full p-2 active:bg-rose-neon/50 -rotate-90">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 16 16"
+                class="bounce"
+              >
+                <path
+                  fill="currentColor"
+                  fill-rule="evenodd"
+                  d="M2.22 5.22a.75.75 0 0 0 0 1.06l5.252 5.252a.75.75 0 0 0 1.06 0l5.252-5.252a.75.75 0 1 0-1.06-1.06L8.001 9.94L3.28 5.22a.75.75 0 0 0-1.06 0"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </span>
+          </button>
 
-        <div class="w-[75%] flex-shrink-0 h-full mx-4">
-          <video class="flex" autoplay loop muted playsinline>
-            <source :src="`/video/2_mobile.webm`" type="video/webm" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        <div class="w-[75%] flex-shrink-0 h-full mx-4">
-          <video class="flex" autoplay loop muted playsinline>
-            <source :src="`/video/3_mobile.webm`" type="video/webm" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        <div class="w-[75%] flex-shrink-0 h-full mx-4">
-          <video class="flex" autoplay loop muted playsinline>
-            <source :src="`/video/4_mobile.webm`" type="video/webm" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        <div class="w-[75%] flex-shrink-0 h-full mx-4">
-          <video class="flex" autoplay loop muted playsinline>
-            <source :src="`/video/5_mobile.webm`" type="video/webm" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-        <div class="w-[75%] flex-shrink-0 h-full mx-4">
-          <video class="flex" autoplay loop muted playsinline>
-            <source :src="`/video/6_mobile.webm`" type="video/webm" />
-            Your browser does not support the video tag.
-          </video>
+          <div class="w-[75%] flex-shrink-0 h-full mx-4">
+            <video class="flex" autoplay loop muted playsinline>
+              <source :src="`/video/2_mobile.webm`" type="video/webm" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div class="w-[75%] flex-shrink-0 h-full mx-4">
+            <video class="flex" autoplay loop muted playsinline>
+              <source :src="`/video/3_mobile.webm`" type="video/webm" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div class="w-[75%] flex-shrink-0 h-full mx-4">
+            <video class="flex" autoplay loop muted playsinline>
+              <source :src="`/video/4_mobile.webm`" type="video/webm" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div class="w-[75%] flex-shrink-0 h-full mx-4">
+            <video class="flex" autoplay loop muted playsinline>
+              <source :src="`/video/5_mobile.webm`" type="video/webm" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+          <div class="w-[75%] flex-shrink-0 h-full mx-4">
+            <video class="flex" autoplay loop muted playsinline>
+              <source :src="`/video/6_mobile.webm`" type="video/webm" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
         </div>
       </div>
     </div>
   </div>
-   </div>
 </template>
 
 <script setup>
@@ -194,6 +197,12 @@ const slide1Element = ref(null);
 const footerElement = ref(null);
 let observer = ref(null);
 const emit = defineEmits(["bottom-reached", "top-reached"]);
+
+let isMobile = ref(null);
+
+defineExpose({
+  isMobile,
+});
 
 onMounted(() => {
   ctx.value = gsap.context((self) => {
@@ -281,6 +290,8 @@ onMounted(() => {
   if (footerElement.value) {
     observer.observe(footerElement.value);
   }
+
+  isMobile.value = window.innerWidth < 768;
 });
 
 onUnmounted(() => {
